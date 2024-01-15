9668cb0ef1d651bb5aac952ef3aec7aa1961156a
E-digital1000 <techelvis7@gmail.com>
2024-01-15 14:57:08 +0000

    timing

--- app/views.py
+++ app/views.py
@@ -266,25 +266,25 @@ def semester_delete_view(request, pk):
 def dashboard_view(request):
     return render(request, 'app/dashboard.html')
 
-from django.shortcuts import render_to_response
-from django.template import RequestContext
+#from django.shortcuts import render_to_response
+#from django.template import RequestContext
 
-def handler404(request, exception, template_name="common/404.html"):
-     response = render_to_response("common/404.html")
-     response.status_code = 404
-     return response
-
-
-def handler500(request, *args, **argv):
-     response = render_to_response('common/500.html', {}, context_instance=RequestContext(request))
-     response.status_code = 500
-
-     return response
+#def handler404(request, exception, template_name="common/404.html"):
+#     response = render_to_response("common/404.html")
+#     response.status_code = 404
+#     return response
 
 
-def handler400(request, exception, template_name="common/400.html"):
-     response = render_to_response('common/400.html', context_instance=RequestContext(request))
-     response.status_code = 400
+#def handler500(request, *args, **argv):
+#     response = render_to_response('common/500.html', {}, context_instance=RequestContext(request))
+#     response.status_code = 500
 
-     return response
+#     return response
+
+
+#def handler400(request, exception, template_name="common/400.html"):
+#     response = render_to_response('common/400.html', context_instance=RequestContext(request))
+#     response.status_code = 400
+
+#     return response
 
